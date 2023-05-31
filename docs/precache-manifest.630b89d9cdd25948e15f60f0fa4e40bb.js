2c20eb9537540e1e05e78634472bbc3868be4c24
Pablo Kalomiris <d.rulz@hotmail.com>
2023-05-31 00:27:51 -0300

    docs agregado

---
+++ docs/precache-manifest.630b89d9cdd25948e15f60f0fa4e40bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6448588b91e48256be67b1ac2e9d41bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31fd2f1d0bfca4f227d2",
+    "url": "/static/css/main.402a2278.chunk.css"
+  },
+  {
+    "revision": "72f849d37b4e1ca01e8e",
+    "url": "/static/js/2.5deb2148.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5deb2148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31fd2f1d0bfca4f227d2",
+    "url": "/static/js/main.b9adf755.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.b9adf755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0963fe691459a95666",
+    "url": "/static/js/runtime-main.5ec8b05a.js"
+  }
+]);
